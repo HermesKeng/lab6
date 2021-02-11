aa44bf087c3698ed3900bad89dab835bfaf3427f
HermesKeng <kaining1101@gmail.com>
2021-02-11 13:57:05 -0800

    solve the problem on upload to the cloud

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.get("/project/:id", project.projectInfo);
   cert: fs.readFileSync("cert.pem"),
 };*/
 
-https.createServer(app).listen(app.get("port"), function () {
+http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"));
 });
